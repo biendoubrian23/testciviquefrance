a61b9603685140369659d7da73bfb91650571a5b
brianbiendou23 <brianbiendou@gmail.com>
2025-12-11 13:31:29 +0100

    fix: Corriger un commentaire dans la section des offres découvertes

--- app/tarifs/page.tsx
+++ app/tarifs/page.tsx
@@ -181,7 +181,7 @@ export default function TarifsPage() {
           </div>
         </section>
 
-        {/* Offre découverte - 2 Sessions d'examen blanc */}}
+        {/* Offre découverte - 2 Sessions d'examen blanc */}
         <section className="py-16 lg:py-20 bg-gray-50">
           <div className="max-w-4xl mx-auto px-4 sm:px-6 lg:px-8">
             <div className="text-center mb-10">
