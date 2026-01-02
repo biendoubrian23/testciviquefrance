373a244902f2e8c14af580b92f895da90fd3078e
brianbiendou23 <brianbiendou@gmail.com>
2026-01-02 01:05:43 +0100

    fix: BottomNav toujours visible - z-index 9999 + classe CSS dediee

--- components/dashboard/BottomNav.tsx
+++ components/dashboard/BottomNav.tsx
@@ -39,7 +39,7 @@ export default function BottomNav() {
   };
 
   return (
-    <nav className="lg:hidden fixed bottom-0 left-0 right-0 bg-white border-t border-gray-200 z-50 safe-area-bottom">
+    <nav className="lg:hidden bottom-nav-fixed border-t border-gray-200">
       <div className="flex justify-around items-center h-16">
         {navItems.map((item) => {
           const active = isActive(item.href);
