87a5216da9ee1545bf9e7a9adb4bf8c44914cb71
brianbiendou23 <brianbiendou@gmail.com>
2026-02-02 16:19:37 +0100

    feat: Implement core application layout with global metadata, SEO, analytics, and authentication providers.

--- app/layout.tsx
+++ app/layout.tsx
@@ -883,6 +883,18 @@ export default function RootLayout({
           `}
         </Script>
 
+        {/* Smartlook */}
+        <Script id="smartlook" strategy="afterInteractive">
+          {`
+            window.smartlook||(function(d) {
+            var o=smartlook=function(){ o.api.push(arguments)},h=d.getElementsByTagName('head')[0];
+            var c=d.createElement('script');o.api=new Array();c.async=true;c.type='text/javascript';
+            c.charset='utf-8';c.src='https://web-sdk.smartlook.com/recorder.js';h.appendChild(c);
+            })(document);
+            smartlook('init', '4badc6089313a952efcf93e767c97204ff32d090', { region: 'eu' });
+          `}
+        </Script>
+
         <AnnouncementBanner />
         <PostHogProvider>
           <AuthProvider>{children}</AuthProvider>
